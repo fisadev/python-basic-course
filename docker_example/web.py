8039c981c3a85965f7291c865ecdb50abe48d861
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-01-17 21:44:33 -0300

    Unused import, and bigger titles

--- docker_example/web.py
+++ docker_example/web.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, jsonify
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -8,11 +8,11 @@ start = datetime.now()
 
 @app.route("/")
 def home_page():
-    return "<h3>Demo web page (server started at {start})</h3>".format(start=start)
+    return "<h1>Demo web page (server started at {start})</h1>".format(start=start)
 
 
 @app.route("/save/<thing>/")
 def save(thing):
     with open('/data/things.txt', 'a') as things_file:
         things_file.write("saved {thing} at {time}\n".format(thing=thing, time=datetime.now()))
-    return "<h3>Saved: {thing}</h3>".format(thing=thing)
+    return "<h1>Saved: {thing}</h1>".format(thing=thing)
